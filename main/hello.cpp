1811fea69f788b3e6f12156d9bfbc16b1c6dbd08
nikhilsp242 <nikhilsp242@gmail.com>
2023-02-14 23:39:47 +0530

    changed cpp file

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,9 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
-  cout << "I have made changes";
+  cout << "This is Task-4\n";
+  cout << "I have made changes\n";
   return 0;
 }
